94a96390c6944a96e79bf1871b31a305c87338c3
PayilagamChennai <30644812+PayilagamChennai@users.noreply.github.com>
2017-11-30 23:32:00 +0530

    Add files via upload
    Postfix installation

---
+++ Postfix - Installation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+yum install epel-release -y
+yum install postfix dovecot telnet squirrelmail openssl -y
+mkdir /etc/postfix/ssl
+cd /etc/postfix/ssl
+openssl req -x509 -nodes -newkey rsa:2048 -keyout server.key -out server.crt -nodes -days 365
+cp server.key /etc/pki/tls/private/ca.key
+cp server.crt /etc/pki/tls/certs/ca.crt
+ls /etc/pki/tls/certs
